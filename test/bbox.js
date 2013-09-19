28c2c04ab9a974aaa414a730c7fcdfe3d2214f49
Damon Oehlman <damon.oehlman@gmail.com>
2013-09-19 21:56:37 +1000

    more bbox tests

--- test/bbox.js
+++ test/bbox.js
@@ -12,3 +12,51 @@ test('can create a bbox from a comma separated string of lat and lngs', function
   t.equal(b.max.lat, -27.9935);
   t.equal(b.max.lon, 153.4162);
 });
+
+test('can create a bbox from a space separated string of lat and lngs', function(t) {
+  var b;
+
+  t.plan(5);
+  b = new BBox('-28.1819 153.1453 -27.9935 153.4162');
+  t.ok(b instanceof BBox, 'created BBox');
+  t.equal(b.min.lat, -28.1819);
+  t.equal(b.min.lon, 153.1453);
+  t.equal(b.max.lat, -27.9935);
+  t.equal(b.max.lon, 153.4162);
+});
+
+test('can create a bbox from a comma separated min and max', function(t) {
+  var b;
+
+  t.plan(5);
+  b = new BBox('-28.1819,153.1453', '-27.9935,153.4162');
+  t.ok(b instanceof BBox, 'created BBox');
+  t.equal(b.min.lat, -28.1819);
+  t.equal(b.min.lon, 153.1453);
+  t.equal(b.max.lat, -27.9935);
+  t.equal(b.max.lon, 153.4162);
+});
+
+test('can create a bbox from a space separated min and max', function(t) {
+  var b;
+
+  t.plan(5);
+  b = new BBox('-28.1819 153.1453', '-27.9935 153.4162');
+  t.ok(b instanceof BBox, 'created BBox');
+  t.equal(b.min.lat, -28.1819);
+  t.equal(b.min.lon, 153.1453);
+  t.equal(b.max.lat, -27.9935);
+  t.equal(b.max.lon, 153.4162);
+});
+
+test('can create a bbox from simple object min and max', function(t) {
+  var b;
+
+  t.plan(5);
+  b = new BBox({ lat: -28.1819, lon: 153.1453 }, { lat: -27.9935, lng: 153.4162 });
+  t.ok(b instanceof BBox, 'created BBox');
+  t.equal(b.min.lat, -28.1819);
+  t.equal(b.min.lon, 153.1453);
+  t.equal(b.max.lat, -27.9935);
+  t.equal(b.max.lon, 153.4162);
+});
