5e507478c684c5c07b14fb6b90e92fa59bb5c3e7
Damon Oehlman <damon.oehlman@gmail.com>
2013-09-19 20:21:26 +1000

    flag version as 1.0.0-alpha.1, gendocs

--- index.js
+++ index.js
@@ -12,4 +12,14 @@
   Tile5 into the original geojs library, and I'm incorporating a lot of
   what I've learnt along the way into this rebuilt from scratch version
   of the libary.
+
+  ## Installing
+
+  As the `1.x` branch of geojs is not yet published in npm you will need to
+  npm install it using a `git://` url at present:
+
+  ```
+  npm install git://github.com/DamonOehlman/geojs.git#1.x --save
+  ```
+  
 **/
